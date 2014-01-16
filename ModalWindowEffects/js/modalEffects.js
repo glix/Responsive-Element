55bf8a66d1fefa660e083de4559cfb253947e2cd
Danny Glix <danny@dannyglix.com>
2014-01-16 13:27:28 -0500

    Update modalEffects.js

--- ModalWindowEffects/js/modalEffects.js
+++ ModalWindowEffects/js/modalEffects.js
@@ -1,18 +1,9 @@
-/**
- * modalEffects.js v1.0.0
- * http://www.codrops.com
- *
- * Licensed under the MIT license.
- * http://www.opensource.org/licenses/mit-license.php
- * 
- * Copyright 2013, Codrops
- * http://www.codrops.com
- */
+
 var ModalEffects = (function() {
 
 	function init() {
 
-		var overlay = document.querySelector( '.md-overlay' );
+		var overlay = document.querySelector( '.md-overlap' );
 
 		[].slice.call( document.querySelectorAll( '.md-trigger' ) ).forEach( function( el, i ) {
 
